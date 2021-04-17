69e79de9de8b1bc23ee6d26096c174e92e76ee3a
Brittany Moes <brittanylee@Brittanys-MacBook-Pro.local>
2021-04-16 21:16:08 -0500

    added comments section

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
+// Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
+
+
+
+router.use((req, res) => {
+    res.status(404).send('<h1>😝 404 Error!</h1>');
+});
 
 module.exports = router;
